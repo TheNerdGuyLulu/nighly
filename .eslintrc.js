a781d1df90ccd6b10aa404b92a682eeccc494e7b
Luís Chaves <luis.chaves@netguru.com>
2025-02-02 14:39:13 +0100

    chore: configure yarn and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'simple-import-sort/imports': [
+      1,
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^react', '/^react-(native)$/', '^@?\\w'],
+          ['^app/[^/]+(/.*|$)'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+  },
 };
