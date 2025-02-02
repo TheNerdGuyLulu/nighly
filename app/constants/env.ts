d0fc1d55b5119247099d9e758010b1bb91f83e19
Luís Chaves <luis.chaves@netguru.com>
2025-02-02 17:25:50 +0100

    chore: add envs

---
+++ app/constants/env.ts
@@ -0,0 +1,7 @@
+/*
+  The variables here could/should be populated using react-native-config or similar
+  This would allow different backend environments (staging, production, etc)
+ */
+export const ENV = {
+  api: 'https://technology.lastminute.com/api/hotel.json',
+} as const;
