059efa33b757dfb3fead113836fc7fddf8340a44
Luís Chaves <luis.chaves@netguru.com>
2025-02-03 19:57:15 +0100

    feat: hide splashscreen after navigation is ready

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import BootSplash from 'react-native-bootsplash';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {RootNavigator} from './navigation';
 
+const onNavigationReady = () => {
+  BootSplash.hide({fade: true});
+};
+
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer onReady={onNavigationReady}>
         <RootNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
